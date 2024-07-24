aa821b448872cd6e4662c6cb960b38be85c8d49a
Shubham Vanarse <100429473+shubs03@users.noreply.github.com>
2024-07-24 11:30:35 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,97 @@
+var btn = document.querySelector('.button')
+var content = document.querySelector('.btn-content')
+
+function speak(text){
+    const text_speak = new SpeechSynthesisUtterance(text);
+    text_speak.lang='hi-IN';
+    text_speak.rate = 1;
+    text_speak.volume = 2;
+    text_speak.pitch = 1;
+
+    window.speechSynthesis.speak(text_speak);
+}
+
+function wishMe(){
+    var day = new Date();
+    var hour = day.getHours();
+
+    if(hour>=0 && hour<12){
+        speak("Good Morning Sir...")
+    }
+    else if(hour>12 && hour<17){
+        speak("Good Afternoon Sir...")
+    }
+    else{
+        speak("Good Evening Sir...")
+    }
+}
+
+window.addEventListener('load', ()=>{
+    speak("Initializing Iris....");
+    wishMe();
+});
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+
+const recognition = new SpeechRecognition();
+
+recognition.onresult = (event)=>{
+    const currentIndex = event.resultIndex;
+    const transcript = event.results[currentIndex][0].transcript;
+    content.textContent = transcript;
+    takeCommand(transcript.toLowerCase());  
+} 
+
+btn.addEventListener('click', ()=>{
+    content.textContent = "Listening...."
+    recognition.start();
+})
+
+function takeCommand(message){
+      if(message.includes("open google")){
+        window.open("https://google.com", "_blank");
+        speak("Opening Google...")
+    }
+    else if(message.includes("open spotify")){
+        window.open("https://open.spotify.com","_blank")
+        speak("Opening Spotisfy...")
+    }
+    else if(message.includes("open facebook")){
+        window.open("https://facebook.com" , "_blank")
+        speak("Opening Facebook...")
+    }
+    else if(message.includes("open youtube")){
+        window.open("https://youtube.com" , "_blank")
+        speak("Opening Youtube...")
+    }
+    else if(message.includes("what is") || message.includes("who is") || message.includes("what are")){
+        window.open(`https://www.google.com/search?q=${message.replace(" ","+")}`,"_blank ")
+        const finalText = ("This is what i found something regarding " + message)
+        speak(finalText)
+    }
+    else if(message.includes("wikipedia")){
+         window.open(`https://en.wikipedia.org/wiki/${message.replace("wikipedia", "")}`, "_blank")
+         const finalText = "Here is the information about" + message
+         speak(finalText)
+    }
+    else if(message.includes('time')) {
+        const time = new Date().toLocaleString(undefined, {hour: "numeric", minute: "numeric"})
+        const finalText = time;
+        speak(finalText);
+    }
+    else if(message.includes('date')) {
+        const date = new Date().toLocaleString(undefined, {month: "short", day: "numeric"})
+        const finalText = date;
+        speak(finalText);
+    }   
+    else if(message.includes('calculator')){
+        window.open("Calculator:///")
+        const finalText = "Opening Calculator";
+        speak(finalText);
+    }
+    else {
+        //window.open(`https://www.google.com/search?q=${message}`,"_blank");
+        const finalText = " prathamesh mala mhanto madi i dhavli mi ata tay talu" ;
+        speak(finalText);
+    }
+}
